96422ab0204eab1a8985267e466e87d1c76dfd09
Aiden Lee <aiden@musefind.com>
2018-07-14 09:12:07 -0700

    added theme provider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
 import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
 
@@ -8,16 +10,23 @@ import Home from './containers/Home';
 
 import './App.css';
 
+const muiTheme = createMuiTheme({
+  typography: {
+    fontFamily: "'Nunito Sans', sans-serif",
+    fontWeight: 300
+  }
+});
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, {}, composeEnhancers(
-  applyMiddleware(ReduxThunk)
-));
+const store = createStore(reducers, {}, composeEnhancers(applyMiddleware(ReduxThunk)));
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Home />
+        <MuiThemeProvider theme={muiTheme}>
+          <Home />
+        </MuiThemeProvider>
       </Provider>
     );
   }
