f45213339a01b2f566581e9595d179c7d36f0595
Jonny Kalambay <jonnyk20@gmail.com>
2018-07-13 22:17:31 -0700

    partly implemented action

--- src/reducers/User.js
+++ src/reducers/User.js
@@ -1,10 +1,14 @@
 
+import { CHANGE_NAME } from '../actions/types';
+
 const initialState = {
   name: 'Aiden'
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case CHANGE_NAME:
+      return { ...state, name: action.payload }
     default:
       return state;
   }
