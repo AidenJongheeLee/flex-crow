4028080be7a9bda21fe7dfc1129a4aa4197fa5f2
Jonny Kalambay <jonnyk20@gmail.com>
2018-07-14 11:21:52 -0700

    conflict fix

---
+++ src/containers/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import{ connect } from 'react-redux';
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        SuccessFully Rendered Dashboard
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
