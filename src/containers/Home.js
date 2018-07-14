c5b429d4b750a7cac8f048e66e33c26a12331cdd
Aiden Lee <aiden@musefind.com>
2018-07-14 08:32:19 -0700

    test action

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { changeName } from '../actions';
 
 class Home extends Component {
   render() {
     console.log(this.props);
     return (
       <div>
+        <button
+          onClick={() => {
+            this.props.changeName('Jonny');
+          }}>
+          {this.props.name}
+        </button>
         SuccessFully Rendered Home
       </div>
     );
   }
-};
+}
 
 const mapStatetoProps = state => {
   return {
     name: state.user.name
-  }
+  };
 };
 
-export default connect(mapStatetoProps, {})(Home);
+export default connect(
+  mapStatetoProps,
+  { changeName }
+)(Home);
