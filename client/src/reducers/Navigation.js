9c7551418bc7039fd87d815ecaab3c55d55ea77e
Jonny Kalambay <jonnyk20@gmail.com>
2018-07-14 12:15:19 -0700

    Wired navigation reducer

---
+++ client/src/reducers/Navigation.js
@@ -0,0 +1,14 @@
+import { CHANGE_TAB } from '../actions/types';
+
+const initialState = {
+  currentTab: 1,
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case CHANGE_TAB:
+      return { ...state, currentTab: action.payload };
+    default:
+      return state;
+  }
+};
